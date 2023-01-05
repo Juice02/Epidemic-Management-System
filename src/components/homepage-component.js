42d4f31049a7dfe9efcc72143bb05da44d8641d2
Juice02 <tejasraghavendra.tr@gmail.com>
2023-01-06 01:42:47 +0530

    case counter not working

--- src/components/homepage-component.js
+++ src/components/homepage-component.js
@@ -1,18 +1,42 @@
 import './homepage.css'
 
+import axios from 'axios'
+import { useState,useEffect} from 'react'
+
 
 const TableHeadItem=({ item })=><th>{item.heading}</th>
 const Hostable = () =>{
 
+    const[data,setData]=useState([])
+    async function fetchData() {
+        try {
+          const res = await axios.get('http://localhost:5000/patients');
+          return res.data;
+        } catch (err) {
+          console.log(err);
+        }
     
-
+      }
+    
+      useEffect(() => {
+        fetchData()
+          .then(data => {
+            setData(data)
+          })
+        
+      },[])
+     
     const column = [
         { heading: 'Active', value: '' },
         { heading: 'Recovered', value: '' },
         { heading: 'Deceased', value: '' }
       ]
       
-      
+      for(var i=0;i<=data.length;i++)
+        {
+            console.log(data[i])
+        }
+     
       return (
         <div className="App">
           <h1>Case Counter</h1>
@@ -24,7 +48,7 @@ const Hostable = () =>{
                 </tr>
             </thead>
             <tbody>
-                <td>{}</td>
+                <td>lol</td>
                 <td>lol</td>
                 <td>lol</td>
             </tbody>
